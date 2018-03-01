5d486338372c3d0046c11266b6b8efc99dad2738
MonteC73 <jarekucinski@gmail.com>
2018-03-01 21:09:40 +0100

    Building the Game Interface

--- src/App.js
+++ src/App.js
@@ -2,6 +2,66 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Stars = (props) => {
+  const numberOfStars = 1 + Math.floor(Math.random()*9);
+
+  let stars = [];
+  for(let i=0; i<numberOfStars; i++) {
+    stars.push(<i key={i} className="fa fa-star"></i>);
+  }
+  return (
+    <div className="col-md-5">
+      {stars}
+    </div>
+  );
+}
+
+const Button = (props) => {
+  return (
+    <div className="col-md-2">
+      <button>=</button>
+    </div>
+  );
+}
+
+const Answer = (props) => {
+  return (
+    <div className="col-md-5">
+      ...
+    </div>
+  );
+}
+
+const Numbers = (props) => {
+  const arrayOfNumbers = [1,2,3,4,5,6,7,8,9];
+
+  return (
+    <div className="card text-center">
+      {arrayOfNumbers.map((number, i) => 
+      <span kye={i}>{number}</span>
+      )}
+    </div>
+  );
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h3>Play Nine</h3>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>
+        <br />
+        <Numbers />
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -10,12 +70,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Game />
       </div>
     );
   }
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
